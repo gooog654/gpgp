cf8056a73b56e0906f8b4bbebffaec6f1cbe4e17
gooog654 <131779899+gooog654@users.noreply.github.com>
2023-04-25 14:26:20 +0100

    Update cas1.cpp

--- cas1.cpp
+++ cas1.cpp
@@ -4,18 +4,19 @@ int main () {
 	printf (" entrez lordre de la matrice:");
 	scanf ("%d",&n);
 	int M[n][n];
-	for (int k=0;k<n;k++)
-	{ for (int i=0,j=k;j>=0;i++,j--){
+	int k , i , j ; 
+	for ( k=0;k<n;k++)
+	{ for ( i=0,j=k;j>=0;i++,j--){
 		M[j][i]=mat++;
 		}
     }
-    for(int k=1;k<n;k++){
-    	for(int i=k,j=n-1;i<n;i++,j--){
+    for( k=1;k<n;k++){
+    	for( i=k,j=n-1;i<n;i++,j--){
     		M[j][i] = mat++;
 		}
 	}
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
+	for( i=0;i<n;i++){
+		for( j=0;j<n;j++){
 			printf("%d\t",M[i][j]);
 		}
 		printf("\n");
